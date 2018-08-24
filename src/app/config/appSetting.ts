6b2415bee3811fb39183c2fb3eec0ecf92d75f9b
ripsildeveloper <ripsildeveloper3@gmail.com>
2018-08-24 18:54:13 +0530

    changes in home page

--- src/app/config/appSetting.ts
+++ src/app/config/appSetting.ts
@@ -1,8 +1,8 @@
 export const AppSetting: AppSettingType = {
     awsServiceUrl: 'http://ec2-13-126-16-163.ap-south-1.compute.amazonaws.com:3001/',
     local3010BuyerServiceUrl: 'http://localhost:3011/',
-  // serviceUrl: 'https://rinteger.com/service/',
-       serviceUrl: 'http://localhost:3011/',
-    adminServiceUrl: 'http://localhost:3012/'
-  //   adminServiceUrl: 'https://rinteger.com/adminservice/'
+  serviceUrl: 'https://rinteger.com/service/',
+  //      serviceUrl: 'http://localhost:3011/',
+  //     adminServiceUrl: 'http://localhost:3012/'
+  adminServiceUrl: 'https://rinteger.com/adminservice/'
 };
