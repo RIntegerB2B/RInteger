71c44420c7d2c67bd85f9670168788ebe4c5454e
Vikasini <vikasininithi@gmail.com>
2018-10-28 10:49:22 +0530

    changes in contact form

--- src/app/home/contact/contact.component.ts
+++ src/app/home/contact/contact.component.ts
@@ -5,6 +5,8 @@ import { CustomerQuery} from './contact.model';
 import {ContactService} from './contact.service';
 import {mobileNumber} from '../../shared/validation';
 import {DashBoardService} from '../../home/dashboard/dashboard.service';
+import { LocalStorageService } from 'ngx-webstorage';
+import {MatSnackBar} from '@angular/material';
 
 @Component({
   selector: 'app-contact',
@@ -12,35 +14,52 @@ import {DashBoardService} from '../../home/dashboard/dashboard.service';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  email: any;
   onSendInForm: FormGroup;
   userQuery: CustomerQuery ;
-  Name;
+  userName;
   mobileNo;
-  constructor(private fb: FormBuilder, private router: Router, private contactService: ContactService,
+  message;
+  action;
+  constructor(private fb: FormBuilder, private router: Router, private localStorageService: LocalStorageService,
+    public snackBar: MatSnackBar,
+    private contactService: ContactService,
     private dashboardService: DashBoardService ) { }
 
   ngOnInit() {
     this.dashboardService.hideMenuTransparent();
     this.createForm();
+    this.checkData();
   }
   createForm() {
     this.onSendInForm = this.fb.group({
       mobileNumber: ['', mobileNumber ],
       name: ['', Validators.required],
-      typeYourMessage: ['', Validators.required]
+      emailId: [''],
+      typeYourMessage: ['']
     });
   }
+  checkData() {
+    this.mobileNo = this.localStorageService.retrieve('mobileno');
+    this.userName = this.localStorageService.retrieve('name');
+    this.email = this.localStorageService.retrieve('emailId');
+  }
   sendSubmit(onSendInForm: FormGroup) {
+    this.message = 'RInteger Team will contact you shortly';
     this.userQuery = new CustomerQuery(
       onSendInForm.controls.name.value,
       onSendInForm.controls.mobileNumber.value,
       onSendInForm.controls.typeYourMessage.value
     );
     this.contactService.addQuery(this.userQuery).subscribe(data => {
+      this.snackBar.open(this.message, this.action, {
+        duration: 3000,
+      });
     }, error => {
       console.log(error);
     });
   this.onSendInForm.reset();
+  this.router.navigate(['/welcome']);
   }
 
 }
