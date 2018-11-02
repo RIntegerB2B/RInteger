77f6ee8b07ac32716c196ddd21169d8f1970c1a1
ripsildeveloper <ripsildeveloper3@gmail.com>
2018-11-02 13:10:59 +0530

    changes in carousel

--- src/app/home/banner/banner.component.ts
+++ src/app/home/banner/banner.component.ts
@@ -6,24 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./banner.component.css']
 })
 export class BannerComponent implements OnInit {
- showIndicator = true;
- fullImages = [ '../../../assets/images/services/silder1.jpg',
-     '../../../assets/images/services/silder2.jpg' ,
-     '../../../assets/images/services/silder3.jpg',
-      '../../../assets/images/services/silder4.jpg' ,
-     '../../../assets/images/services/silder5.jpg',
-     '../../../assets/images/services/silder6.jpg',
-     '../../../assets/images/services/silder7.jpg',
-     '../../../assets/images/services/silder8.jpg' ,
-     '../../../assets/images/services/silder9.jpg',
-     '../../../assets/images/services/silder10.jpg',
-      ] ;
-  myCarouselOptions = { items: 5, dots: true, nav: true, autoplay: true,
-     autoplaySpeed: true, loop: true};
 constructor() {
-}
-switchIndicator(): void {
-  this.showIndicator = !this.showIndicator;
 }
 ngOnInit() {
 
