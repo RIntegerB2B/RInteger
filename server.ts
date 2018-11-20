d813dc0b162d179efeef5cd8b76e88fdcc0528d3
ripsildeveloper <ripsildeveloper3@gmail.com>
2018-11-20 11:27:02 +0530

    changes in seo page

--- server.ts
+++ server.ts
@@ -3,37 +3,28 @@ import 'zone.js/dist/zone-node';
     import {enableProdMode} from '@angular/core';
     import {ngExpressEngine} from '@nguniversal/express-engine';
     import {provideModuleMap} from '@nguniversal/module-map-ngfactory-loader';
-    
     import * as express from 'express';
     import * as bodyParser from 'body-parser';
     import * as cors from 'cors';
     import * as compression from 'compression';
-    
     import {join} from 'path';
-    
     enableProdMode();
-    
     export const app = express();
-    
     app.use(compression());
     app.use(cors());
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
-    
     // const DIST_FOLDER = join(process.cwd(), 'dist');
-    
     const {AppServerModuleNgFactory, LAZY_MODULE_MAP} = require('./dist/server/main');
-    
     app.engine('html', ngExpressEngine({
       bootstrap: AppServerModuleNgFactory,
       providers: [
         provideModuleMap(LAZY_MODULE_MAP)
       ]
     }));
-    
+
     app.set('view engine', 'html');
     app.set('views', './dist/browser');
-    
     app.get('/redirect/**', (req, res) => {
       const location = req.url.substring(10);
       res.redirect(301, location);
@@ -42,7 +33,7 @@ import 'zone.js/dist/zone-node';
     app.get('*.*', express.static('./dist/browser', {
       maxAge: '1y'
     }));
-    
+
     app.get('/*', (req, res) => {
       res.render('index', {req, res}, (err, html) => {
         if (html) {
@@ -53,4 +44,3 @@ import 'zone.js/dist/zone-node';
         }
       });
     });
-    
